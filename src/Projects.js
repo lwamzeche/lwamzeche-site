e6d52c2959b83284517db6339214d75581a42f9a
lwamzemikael <lwam@sentience.rocks>
2024-07-11 11:08:47 +0900

    projects edited

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,6 @@ import "./Projects.css";
 import steamDexImage from "./images/steam.png";
 import Roomie from "./images/LOGO.png";
 import MathPark from "./images/Math.png";
-import TicketEase from "./images/TicketEase.png";
 import TentuPlay from "./images/TentuPlay.png";
 import PianoRing from "./images/PianoRing.png";
 import EMS from "./images/EMS.jpeg";
@@ -30,7 +29,7 @@ const projects = [
   },
 
   {
-    id: 1,
+    id: 3,
     title: "EMS for touchscreens in VR",
     description:
       "In this individual reaserch I am working on replication a work that has been done on providing haptics to walls & heavy objects in virtual reality by means of electrical muscle stimulation” then Working on utilizing EMS to provide screen haptic feedback to improve the usability of a virtual touchscreen in VR.",
@@ -39,7 +38,7 @@ const projects = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Steam Dex",
     description:
       "A website that analyzes various indie games from Steam, providing details such as price, budget, revenue, review count, and tags. It enables users to search for games by name, price, revenue, and tags.",
@@ -47,7 +46,7 @@ const projects = [
     projectUrl: "https://steam-dex.com/",
   },
   {
-    id: 3,
+    id: 5,
     title: "Math Park",
     description:
       "A web application that utilized a machine learning model trained on TensorFlow.js to accurately classify handwritten digits",
@@ -56,19 +55,10 @@ const projects = [
   },
 
   {
-    id: 4,
-    title: "TicketEase-NFT",
-    description:
-      "Me and my teammates developed a web3 app using solidity, Truffle, Vercel and Sepolia for our Blockchain class in KAIST",
-    imageUrl: TicketEase,
-    projectUrl: "https://ticket-ease.vercel.app/",
-  },
-
-  {
-    id: 5,
+    id: 6,
     title: "TentuPlay JS SDK",
     description:
-      "As a software engineer at Sentinece, I am developing API methods for the Tentuplay JS SDK",
+      "As a software engineer at Sentinece, I have developed API methods for the Tentuplay JS SDK",
     imageUrl: TentuPlay,
     projectUrl: "https://docs.tentuplay.io/js/en/latest/",
   },
